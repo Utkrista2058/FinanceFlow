cdfe844e6bc6b5a0ff109b04f2810f94e2ba7c71
Utkrista Parajuli <utkrista@sst.asia>
2025-11-12 16:36:32 +0545

    Changes in api.js added base url to live link

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://localhost:7230/api'  
+  // baseURL: 'https://localhost:7230/api'  
+  baseURL: 'https://financeflow-2esa.onrender.com/api'
 });
 
 
